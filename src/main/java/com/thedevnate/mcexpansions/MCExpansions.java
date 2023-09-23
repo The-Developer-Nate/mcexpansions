5201f3a88c8e59ad6181347237b8641226b638c1
EXNate <nlee32052@gmail.com>
2023-09-22 21:42:14 -0400

    Initial Commit.

---
+++ src/main/java/com/thedevnate/mcexpansions/MCExpansions.java
@@ -0,0 +1,13 @@
+package com.thedevnate.mcexpansions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MCExpansions implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/thedevnate/mcexpansions/client/MCExpansionsClient.java
@@ -0,0 +1,13 @@
+package com.thedevnate.mcexpansions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCExpansionsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
